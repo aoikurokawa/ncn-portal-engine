e531cef0c66ce2d38effa11b803126196d13d4b4
Aoi <ht.so.ik0102@gmail.com>
2022-03-07 22:17:05 +0700

    create some function

--- programs/mycalculatordapp/src/lib.rs
+++ programs/mycalculatordapp/src/lib.rs
@@ -5,4 +5,14 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 #[program]
 pub mod mycalculatordapp {
     use super::*;
+
+    pub fn create(ctx: Context<Create>, init_message: String) -> ProgramResult {
+
+        Ok(())
+    }
+
+    pub fn add(ctx: Context<Addition>, num1: i64, num2: i64) -> ProgramResult {
+
+        Ok(())
+    }
 }
