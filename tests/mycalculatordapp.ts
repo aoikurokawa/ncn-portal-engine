8f7b61c7e2b3eb01e0f0a8c1f8be80d7b7cc7d89
Aoi <ht.so.ik0102@gmail.com>
2022-03-08 07:21:56 +0700

    write test case for add and create function

--- tests/mycalculatordapp.ts
+++ tests/mycalculatordapp.ts
@@ -1,5 +1,5 @@
 import assert from "assert";
-import anchor from "@project-serum/anchor";
+import anchor, { web3 } from "@project-serum/anchor";
 
 const { SystemProgram } = anchor.web3;
 
@@ -10,10 +10,37 @@ describe("mycalculatordapp", () => {
   const calculator = anchor.web3.Keypair.generate();
   // @ts-ignore
   const program = anchor.workspace.Mycalculatordapp;
+  
+  let _calculator: web3.Keypair;
 
-  it("Creates a calculator", async () => {});
+  it("Creates a calculator", async () => {
+    await program.rpc.create("Welcome to Solana", {
+      accounts: {
+        calculator: calculator.publicKey,
+        user: provider.wallet.publicKey,
+        systemProgram: SystemProgram.programId,
+      },
+      signers: [calculator],
+    });
 
-  it("Adds two numbers", async () => {});
+    const account = await program.account.calculator.fetch(calculator.publicKey);
+    assert.ok(account.greeting === "Welcome to Solana");
+    _calculator = calculator;
+  });
+
+  it("Adds two numbers", async () => {
+    const calculator = _calculator;
+
+    await program.rpc.add(new anchor.BN(2), new anchor.BN(3), {
+      accouts: {
+        calculator: calculator.publicKey
+      },
+    });
+
+    const account = await program.account.calculator.fetch(calculator.publicKey);
+    assert.ok(account.result.eq(new anchor.BN(5)));
+    assert.ok(account.greeting === "Welcome to Solana");
+  });
 
   it("Multiplies two numbers", async () => {});
 
